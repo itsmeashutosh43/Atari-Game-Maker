a796a3911477b303a6b2f07180aedf837441082e
ashutosh <chapagainashutosh8@gmail.com>
2022-09-28 14:40:12 -0400

    Adding draggable functionality

--- src/game-runner/view/circleDrawable.ts
+++ src/game-runner/view/circleDrawable.ts
@@ -4,6 +4,7 @@ import { position } from "../model/objects/iposition";
 export class CircleDrawable {
   draw(size: Size, pos: position): void {
     // get the ctx from some constant file
+    // reference contrxt by CanvasLayout.getInstance()
     // draw using size and position
   }
 }
